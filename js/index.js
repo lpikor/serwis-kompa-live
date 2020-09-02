c337828b403ff2d6e3f1b994cdb37e270aa112da
Łukasz Pikor <pikorlukasz@gmail.com>
2020-09-02 12:11:40 +0200

    Remove overflow from active nav

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
+const body = document.querySelector('body');
 
 const handleClick = () => {
     hamburger.classList.toggle('hamburger--active');
     nav.classList.toggle('nav--active');
+    body.classList.toggle('body--nav');
 }
 
 hamburger.addEventListener('click', handleClick);
